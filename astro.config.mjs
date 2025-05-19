8983e96344145ccb09610eec0d5b169f36acf864
Andersonlara15 <148564761+Andersonlara15@users.noreply.github.com>
2025-05-18 20:19:16 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  sitie: 'https://andersonlara15.github.io',
   integrations: [
     react(),
     tailwind({
